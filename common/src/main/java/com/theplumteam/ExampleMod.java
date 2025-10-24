806e5a9219864a698f1526f0a9bf8c8028b8475c
AkaNebur <rubengalandiaz@gmail.com>
2025-10-24 16:31:27 +0200

    Start project

---
+++ common/src/main/java/com/theplumteam/ExampleMod.java
@@ -0,0 +1,9 @@
+package com.theplumteam;
+
+public final class ExampleMod {
+    public static final String MOD_ID = "blockpops";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
